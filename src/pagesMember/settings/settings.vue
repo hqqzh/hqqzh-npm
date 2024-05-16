cff9589f8847cd04620a6549348815930c1b24d7
念旧的俗人 <3154309768@qq.com>
2024-05-16 15:37:07 +0800

    1

--- src/pagesMember/settings/settings.vue
+++ src/pagesMember/settings/settings.vue
@@ -19,7 +19,6 @@ const onLogout = () => {
 
 <template>
   <view class="viewport">
-    <!-- 列表1 -->
     <view class="list" v-if="memberStore.profile">
       <navigator url="/pagesMember/address/address" hover-class="none" class="item arrow">
         我的收货地址
